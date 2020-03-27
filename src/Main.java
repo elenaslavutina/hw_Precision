986900238e5468cc1959e0d7d0bbfc5e0b04b47c
elenaslavutina <elena.slavutina2013@gmail.com>
2020-03-27 15:49:13 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        double regularBonus = 0.3;
+        double specialBonus = 0.6;
+        double totalBonus = regularBonus + specialBonus;
+        System.out.println(totalBonus);
+    }
+}
